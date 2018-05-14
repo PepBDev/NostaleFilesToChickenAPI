ba1f1a07694266918f2e7f2bd95f4ba697d5f317
PepBD <pepbdev@gmail.com>
2018-05-14 19:18:35 +0200

    Finished parsing

---
+++ src/NostaleFilesToChickenAPI/ItemParser/Parser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NostaleFilesToChickenAPI.ItemParser
+{
+    class Parser
+    {
+        public string[] getBlocks(string path)
+        {
+            string allText = File.ReadAllText(path);
+            string[] blocks = allText.Split(new string[] { "#========================================================" }, StringSplitOptions.None);
+            return blocks;
+        }
+        public Dictionary<string, string[]> getValues(string block)
+        {
+            Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+            string[] fields = Regex.Split(block, "\t");
+            var foos = new List<string>(fields);
+            foos.RemoveAt(0);
+            string text = String.Join("\t", foos.ToArray());
+            string[] values = Regex.Split(text, "\r");
+            values = values.Take(values.Count() - 1).ToArray();
+            foreach (string value in values)
+            {
+                string[] fieldName = Regex.Split(value, "\t");
+                fieldName = fieldName.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                var newList = new List<string>(fieldName);
+                if(newList.Count >= 1)
+                {
+                    newList.RemoveAt(0);
+                    string[] valueList = newList.ToArray();
+                    dictionary.Add(fieldName[0], valueList);
+                }
+                else
+                {
+                }
+
+            }
+            return dictionary;
+        }
+        
+    }
+}
--- src/NostaleFilesToChickenAPI/Program.cs
+++ src/NostaleFilesToChickenAPI/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NostaleFilesToChickenAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string path = "c:\\Item.dat";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Item.dat path is not well!");
+            }
+            Console.Write("\n");
+            ItemParser.Parser Parser = new ItemParser.Parser();
+            string[] blocks = Parser.getBlocks(path);
+            Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+            foreach (string block in blocks)
+            {
+                dictionary = Parser.getValues(block);
+                if (dictionary.ContainsKey("VNUM"))
+                {
+                    string[] test = dictionary["VNUM"];
+                    Console.Write("VNUM: " + String.Join("  ", dictionary["VNUM"]) + "\n");
+                }
+                else
+                {
+                    Console.Write("All VNUMS parsed");
+                }
+            }
+
+            Console.WriteLine("Item.dat loaded");
         }
     }
 }
